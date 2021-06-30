ae3b385b232d2cf3f48b11d189025df8a1907136
BAEK DONGWOO <qorehddn123@naver.com>
2021-06-30 16:58:29 +0900

    logged 구현

--- components/auth/Login.js
+++ components/auth/Login.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { View, Button, TextInput } from 'react-native';
+import firebase from 'firebase';
+export class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+        };
+        this.onSignUp = this.onSignUp.bind(this);
+    }
+    onSignUp() {
+        const { email, password } = this.state;
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(email, password)
+            .then((result) => {
+                console.log(result);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
 
-import { Text, View } from 'react-native';
+    render() {
+        return (
+            <View>
+                <TextInput
+                    placeholder="email"
+                    onChangeText={(email) => this.setState({ email })}
+                />
+                <TextInput
+                    placeholder="password"
+                    secureTextEntry={true}
+                    onChangeText={(password) => this.setState({ password })}
+                />
 
-const Login = () => {
-    return <Text>Login</Text>;
-};
+                <Button onPress={() => this.onSignUp()} title="Sign In" />
+            </View>
+        );
+    }
+}
 
 export default Login;
